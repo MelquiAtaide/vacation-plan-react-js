8214c9f215a0fdfec3e20196334da2d59f1bb3ff
Melqui Ataide <melqueataide@gmail.com>
2024-03-13 22:50:00 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Dashboard from './components/layouts/dashboard';
+
+function App() {
+  return (
+    <div className="page">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/home/inde.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './home.css';
+// import { Button } from '@mui/material';
+// import foto from '../../assets/imgs/el_gato.png'
+// import { HiDownload } from "react-icons/hi";
+
+function Home() {
+    return (
+        <div class="home">
+            {/* <img src={foto} alt="foto_perfil" class="foto"/> */}
+            <div className="list-container">
+                <h2>2024</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
